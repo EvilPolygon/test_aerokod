6f8cd5dbf3238aba2f8f5128b848bbfc29ee4f06
EvilPolygon <59900883+EvilPolygon@users.noreply.github.com>
2025-02-10 22:19:44 +0500

    correct modal

--- src/components/complex/Modal.tsx
+++ src/components/complex/Modal.tsx
@@ -33,30 +33,34 @@ export default function Modal ({setModalOpen}) {
   };
 
   return (
-    <div className={'flex gap-[8px] items-start flex-col border border-slate-200 shadow-sm m-[8px] p-[8px] bg-blue-100'}>
-      <div>
-        <span className={'mr-[8px]'}>{'Название'}</span>
-        <Input name={'title'} onChange={inputHandler} />
-      </div>
-      <div className={'flex'}>
-        <span className={'mr-[8px]'}>{'Описание'}</span>
-        <textarea
-          className={'bg-white text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow'}
-          onChange={(e) => setNewTaskData({...newTaskData, description: e.target.value})}
-        />
-      </div>
-      <div>
-        <span className={'mr-[8px]'}>{'Время на выполнение'}</span>
-        <div>
-          <span className={'mr-[8px]'}>{'ч'}</span>
-          <Input name={'hours'} onChange={inputHandler} />
-          <span className={'mx-[8px]'}>{'м'}</span>
-          <Input name={'minutes'} onChange={inputHandler} />
+    <div className={'absolute inset-0 bg-slate-200/50'}>
+      <div className={'float-right'}>
+        <div className={'flex gap-[8px] items-start flex-col border border-slate-200 shadow-sm m-[8px] p-[8px] bg-blue-100'}>
+          <div>
+            <span className={'mr-[8px]'}>{'Название'}</span>
+            <Input name={'title'} onChange={inputHandler} />
+          </div>
+          <div className={'flex'}>
+            <span className={'mr-[8px]'}>{'Описание'}</span>
+            <textarea
+              className={'bg-white text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow'}
+              onChange={(e) => setNewTaskData({...newTaskData, description: e.target.value})}
+            />
+          </div>
+          <div>
+            <span className={'mr-[8px]'}>{'Время на выполнение'}</span>
+            <div>
+              <span className={'mr-[8px]'}>{'ч'}</span>
+              <Input name={'hours'} onChange={inputHandler} />
+              <span className={'mx-[8px]'}>{'м'}</span>
+              <Input name={'minutes'} onChange={inputHandler} />
+            </div>
+          </div>
+          <div className={'flex gap-[16px]'}>
+            <Button onClick={createTaskHandler}>{'Создать'}</Button>
+            <Button color={'danger'} onClick={closeModalHandler}>{'Закрыть'}</Button>
+          </div>
         </div>
-      </div>
-      <div className={'flex gap-[16px]'}>
-        <Button onClick={createTaskHandler}>{'Создать'}</Button>
-        <Button color={'danger'} onClick={closeModalHandler}>{'Закрыть'}</Button>
       </div>
     </div>
   );
